8c63deb72ac7b292a1f5aa5e5b4dfb8af07f8fbc
kylieanyce <kylieanyce@gmail.com>
2021-01-19 15:34:45 -0600

    added layout for data provider

---
+++ scripts/morocco/cities/cityDataProvider.js
@@ -0,0 +1,16 @@
+const cityDataCollection = [
+    {
+        image: "https://cdn.kimkim.com/files/a/images/1f1f7b77e2f5f323a7ebb64e4aa9c81423971b6b/big-d1a809daa1f91b6c59b3e6c7cdeb53f1.jpg",
+        name: "Rabat"
+    },
+    {
+        image: "",
+        name: ""
+    },
+    {
+        image: "",
+        name: ""
+    }
+]
+
+export const 
