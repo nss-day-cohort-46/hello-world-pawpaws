8b84f075cec6c91837f53649a613d4a0571a0c31
Thomas Witherow <twitherow@bellsouth.net>
2021-01-19 15:26:31 -0600

    celebrities

--- scripts/scotland/Cities/City.js
+++ scripts/scotland/Cities/City.js
@@ -1,8 +1,8 @@
 export const City = (city) => {
     return `
         <section class="fish card">
-            <div><img  class="fish__image image--card" src="${city.image}" /></div>
-            <div class="fish__name">${city.name}</div>
+            <div><img  class="city__image image--card" src="${city.image}" /></div>
+            <div class="city__name">${city.name}</div>
         </section>
     `
 }
--- scripts/scotland/Cities/CityDataProvider.js
+++ scripts/scotland/Cities/CityDataProvider.js
@@ -10,15 +10,15 @@ const cityCollection = [
         
     },
     {
-        image: "",
-        name: "",
+        image: "https://media.istockphoto.com/photos/aerial-view-of-aberdeen-typhoon-shelters-and-ap-lei-chau-hong-kong-picture-id1223949557?b=1&k=6&m=1223949557&s=170667a&w=0&h=8mg_k6V_TmM1q-iaH16HcJImmVSJbeVh-ESt-Fg-XJU=",
+        name: "Aberdeen",
         
     },
 ]
 
     console.log()
-    export const useFish = () => {
-        return fishCollection.slice()
+    export const useCity = () => {
+        return cityCollection.slice()
     }
     
     
