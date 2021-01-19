29cc829c8b07781e677bd5236e4c391c4ba6888d
daprieu <daprieu@gmail.com>
2021-01-19 15:07:55 -0600

    making commit to ceck pr

--- scripts/czech/celebrities/CelebritiesDataProvider.js
+++ scripts/czech/celebrities/CelebritiesDataProvider.js
@@ -2,16 +2,16 @@
 
 const celebritiesCollection = () => [
     {
-        image: "",
-        name: ""
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Gregor_Mendel_2.jpg/220px-Gregor_Mendel_2.jpg",
+        name: "Gregor Mendel"
     },
     {
-        image: "",
-        name: ""
+        image: "https://media.npr.org/assets/img/2013/03/08/3274654-sigmund-freud-portrait_custom-74f04f5f6641adcdb203fb3a041b3916f4c5585c-s800-c85.jpg",
+        name: "Sigmund Freud"
     },
     {
-        image: "",
-        name: ""
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Milos_Forman.jpg/220px-Milos_Forman.jpg",
+        name: "Miloš Forman"
     }
 ]
 
@@ -19,3 +19,16 @@ const celebritiesCollection = () => [
 export const useCelebs = () => {
     return celebritiesCollection.slice()
 }
+
+{/* <div class="czechRepublicList__citizensCard">
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Gregor_Mendel_2.jpg/220px-Gregor_Mendel_2.jpg" alt="Gregor Mendel">
+<p>Gregor Mendel</p>
+</div>
+<div class="czechRepublicList__citizensCard">
+<img src="https://media.npr.org/assets/img/2013/03/08/3274654-sigmund-freud-portrait_custom-74f04f5f6641adcdb203fb3a041b3916f4c5585c-s800-c85.jpg" alt="Sigmund Freud">
+<p>Sigmund Freud</p>
+</div>
+<div class="czechRepublicList__citizensCard">
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Milos_Forman.jpg/220px-Milos_Forman.jpg" alt="Miloš Forman">
+<p>Miloš Forman</p>
+</div> */}
