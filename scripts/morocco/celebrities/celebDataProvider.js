ef30bb106fa160e262a0652e339d5d1a638a6e1f
kylieanyce <kylieanyce@gmail.com>
2021-01-20 09:14:45 -0600

    added automation to celebs

--- scripts/morocco/celebrities/celebDataProvider.js
+++ scripts/morocco/celebrities/celebDataProvider.js
@@ -1,14 +1,14 @@
 const dataCollection = [
     {
-        image: "https://cdn.kimkim.com/files/a/images/1f1f7b77e2f5f323a7ebb64e4aa9c81423971b6b/big-d1a809daa1f91b6c59b3e6c7cdeb53f1.jpg",
+        image: "",
         name: "French Montana"
     },
     {
-        image: "https://www.nuflit.com/blog/images/2017/2017-marrakesh-headline.jpg",
+        image: "",
         name: "Robert Purvis"
     },
     {
-        image: "https://www.tetouanclub.com/wp-content/uploads/2019/08/mont-jbel-bouzitoun.jpg",
+        image: "",
         name: "David Guetta"
     }
 ]
