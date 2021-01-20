466c20f303eecbbd16951aaad4493c03babf6ea1
kylieanyce <kylieanyce@gmail.com>
2021-01-20 13:11:54 -0600

    added photos and deleted HTML to celebs.js

--- scripts/morocco/celebrities/celebDataProvider.js
+++ scripts/morocco/celebrities/celebDataProvider.js
@@ -1,14 +1,14 @@
 const dataCollection = [
     {
-        image: "",
+        image: "../../../moroccoImages/french-montana.jpg",
         name: "French Montana"
     },
     {
-        image: "",
+        image: "../../../moroccoImages/Robert_Purvis,_Abolitionist.jpg",
         name: "Robert Purvis"
     },
     {
-        image: "",
+        image: "../../../moroccoImages/David-Guetta-Live.jpg",
         name: "David Guetta"
     }
 ]
