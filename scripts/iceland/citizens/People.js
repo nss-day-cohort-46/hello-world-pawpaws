534b90c24f47265d2b5270f11c1f88e8ff2862ef
samj0516 <samj0516@gmail.com>
2021-01-19 22:13:09 -0600

    closed parentheses

--- scripts/iceland/citizens/People.js
+++ scripts/iceland/citizens/People.js
@@ -2,7 +2,7 @@
 export const Person = (person) => {
     return `
     <div class="icelandList__citizensCard">
-        <img src="${person.image}>
+        <img src="${person.image}">
         <p>${person.name}</p>
     </div>`
 }
