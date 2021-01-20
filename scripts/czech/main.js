7321b8ec875404d61a8149015ab0542d04939778
daprieu <daprieu@gmail.com>
2021-01-19 21:11:09 -0600

    made all files for landmarks and displaying them

--- scripts/czech/main.js
+++ scripts/czech/main.js
@@ -1,7 +1,12 @@
-import { CelebritiesList } from "/scripts/czech/celebrities/celebritiesList.js"
-import { CityList } from "scripts/czech/cities/citiesList.js"
 
+import { CelebritiesList } from "/scripts/czech/celebrities/celebritiesList.js"
+import { CityList } from "/scripts/czech/cities/citiesList.js"
+import { landmarkList } from "/scripts/czech/landmarks/landmarksList.js"
 
 
 CelebritiesList()
-CityList()
+const test = CityList()
+const test2 = landmarkList()
+
+console.log(test)
+console.log(test2)
